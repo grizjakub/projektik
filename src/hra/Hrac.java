4696bdf2fb688685e40f08631b9043503692ee74
Griz <grizjakub6@gmail.com>
2026-01-22 20:45:00 +0100

    promenne, gettery a settery a toString metoda v Lokaci

--- src/hra/Hrac.java
+++ src/hra/Hrac.java
@@ -1,4 +1,13 @@
 package hra;
 
 public class Hrac {
+
+    private Lokace lokace;
+
+
+    public void setLokace(Lokace lokace) {
+        this.lokace = lokace;
+    }
 }
+
+
--- src/hra/Lokace.java
+++ src/hra/Lokace.java
@@ -1,4 +1,32 @@
 package hra;
 
+import java.util.ArrayList;
+
 public class Lokace {
+
+    private String id;
+    private String jmeno;
+    private String popisek;
+    private ArrayList<String> okolni;
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getJmeno() {
+        return jmeno;
+    }
+
+    @Override
+    public String toString() {
+        return "game.Location{" +
+                "id='" + id + '\'' +
+                ", name='" + jmeno + '\'' +
+                ", description='" + popisek + '\'' +
+                ", neighbors=" + okolni +
+                '}';
+    }
 }
+
