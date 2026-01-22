90356e1f43178a92564e9a7cee20b0005e19e4c2
Griz <grizjakub6@gmail.com>
2026-01-22 23:01:47 +0100

    zprovozneni pohybu

--- src/command/Pohyb.java
+++ src/command/Pohyb.java
@@ -1,17 +1,35 @@
 package command;
 
+import hra.HerniData;
 import hra.Hrac;
 
 public class Pohyb extends Command {
+    private String jmeno;
 
-    private Hrac hrac;
-
-    public Pohyb(Hrac hrac) {
-        this.hrac = hrac;
+    public Pohyb(Hrac hrac, String id, HerniData herniData) {
+        super(hrac, herniData);
+        this.jmeno = id;
     }
 
     @Override
-    String jdi(String command) {
-        return "";
+    public String execute() {
+        if (herniData.najdiLokaci(jmeno)) {
+            for (int i = 0; i < herniData.getLokace().size(); i++) {
+                if (herniData.getLokace().get(i).getJmeno().equalsIgnoreCase(jmeno)) {
+                    if (hrac.getAktualniLokace().getOkolni().contains(herniData.getLokace().get(i).getId())) {
+                        hrac.setAktualniLokace(herniData.getLokace().get(i));
+                        return "Aktualni mistnost " + hrac.getAktualniLokace();
+                    }
+                }
+            }
+            return "Lokace: " + jmeno + " není v dosahu";
+        }else {
+            return "Lokace: " + jmeno + " neexistuje";
+        }
+    }
+
+    @Override
+    public boolean exit() {
+        return false;
     }
 }
