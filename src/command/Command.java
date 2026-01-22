a8a41a3bd8fb9a04908d12cf34add84f78567e8b
Griz <grizjakub6@gmail.com>
2026-01-22 21:10:47 +0100

    start a komentar

--- src/command/Command.java
+++ src/command/Command.java
@@ -1,6 +1,6 @@
 package command;
 
 public abstract class Command {
-
+        // Metoda na otevreni nabidkz cesty.
     abstract String jdi(String command);
 }
--- src/hra/Main.java
+++ src/hra/Main.java
@@ -5,5 +5,7 @@ package hra;
 public class Main {
     public static void main(String[] args) {
 
+        Hra hra = new Hra();
+        hra.start();
     }
 }
